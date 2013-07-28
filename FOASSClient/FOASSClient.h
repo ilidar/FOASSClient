2e8d4c611ecdafebffe2474639f3d894b8ed0fd9
chip <kotelovych@mail.ru>
2013-07-28 14:28:34 +0300

    Initial commit;

---
+++ FOASSClient/FOASSClient.h
@@ -0,0 +1,13 @@
+//
+//  FOASSClient.h
+//  FOASSClient
+//
+//  Created by Denys Kotelovych on 28.07.13.
+//  Copyright (c) 2013 DKSoftware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FOASSClient : NSObject
+
+@end
---
+++ FOASSClient/FOASSClient.m
@@ -0,0 +1,13 @@
+//
+//  FOASSClient.m
+//  FOASSClient
+//
+//  Created by Denys Kotelovych on 28.07.13.
+//  Copyright (c) 2013 DKSoftware. All rights reserved.
+//
+
+#import "FOASSClient.h"
+
+@implementation FOASSClient
+
+@end
